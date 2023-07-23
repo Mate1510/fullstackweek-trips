2b29cbd51462dd5e0d37df7e19b4e1558852e31d
Mateus Abreu <mateusabreucn@gmail.com>
2023-07-23 14:50:50 -0300

    feat: creating the login and logout with OAuth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-import Image from 'next/image'
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
+  const { data, status } = useSession();
+
   return (
-    <h1>Teste</h1>
-  )
+    <div className="flex flex-col">
+      <button onClick={() => signIn()}>Sign In</button>
+      <button onClick={() => signOut()}>Sign Out</button>
+
+      <div className="flex flex-col mx-auto mt-4">
+        <h1>Olá, {data?.user?.name}</h1>
+        <Image
+          width={120}
+          height={120}
+          src={data?.user?.image ?? ""}
+          alt="{data?.user?.name}"
+        ></Image>
+      </div>
+    </div>
+  );
 }
